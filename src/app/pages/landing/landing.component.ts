14ef9ac44abaa9e0dfba89928d12b772eb398bb7
Marcio Pezzi <marciopezzi92@gmail.com>
2024-10-23 19:54:55 -0300

    feat: landing-page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FormComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
